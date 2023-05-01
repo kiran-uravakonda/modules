699270e634744e1d03769cd0d32e35434b580176
Kiran-urakonda <kiran123556789@gmail.com>
2023-05-02 00:26:06 +0530

    update code

--- sample.js
+++ sample.js
@@ -1,10 +1,10 @@
 var http=require('http');
 var port=2000;
 http.createServer((req,res)=>{
-    // console.log(req.headers)
+    console.log(req.method)
     res.setHeader('content-Type','text/html')
     res.write("welcome to nodejs")
     res.end()
 }).listen(port,()=>{
     console.log(`server running on ${port}`)
-})
+}) 
