4a27fbfd97e7f17aff33f897257d29b778627d9b
Kiran-urakonda <kiran123556789@gmail.com>
2023-05-02 00:08:00 +0530

    updated code

--- sample.js
+++ sample.js
@@ -1,13 +1,10 @@
-function first(msg,callback)
-{
-    callback();
-        console.log(msg);
-        // callback();
-    
-}
-
-function second()
-{
-    console.log("second number")
-}
-first("first number",second)
+var http=require('http');
+var port=2000;
+http.createServer((req,res)=>{
+    // console.log(req.headers)
+    res.setHeader('content-Type','text/html')
+    res.write("welcome to nodejs")
+    res.end()
+}).listen(port,()=>{
+    console.log(`server running on ${port}`)
+})
